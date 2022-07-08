84f54f25984649c884068f8813df7c9c2c409a89
Trevor Esparza <trevoresparza30@gmail.com>
2022-07-08 11:01:32 -0500

    lunch break

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,67 @@
-import React, { useState } from 'react';
-
+import React, {useState} from 'react';
+import styled from "styled-components";
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+  color: ${props => (props.invalid ? 'red' : 'black')};
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+  background: ${props => (props.invalid ? '#f6d2ea' : 'transparent')};
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #f6f4f5;
+  border-color: #8b005d;
+}
+
+`;
+
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+    const goalInputChangeHandler = event => {
+        if(event.target.value.trim().length > 0){
+            setIsValid(true);
+        }
+        setEnteredValue(event.target.value);
+    };
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
+        props.onAddGoal(enteredValue);
+    };
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+    return (
+        <form onSubmit={formSubmitHandler}>
+            {/* statement below combines temp literal with ternary operator for conditional*/}
+            <FormControl invalid={!isValid}>
+                <label >Course Goal</label>
+                <input type="text" onChange={goalInputChangeHandler}/>
+            </FormControl>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
 };
 
 export default CourseInput;
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import styled from  'styled-components';
+// import './Button.css';
 
-import './Button.css';
+{/*double back ticks after button below, 'tagged temp lit'  the button now becomes a method on the styled object
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+ what we pass between the backticks end up in the button method will return a new button component
+ styled packaged has tags for all html elements */ }
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+
+&:focus {
+  outline: none;
+}
+
+&:hover,
+&:active {
+  background: #ac0e77;
+  border-color: #ac0e77;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+}
+
+`;
+
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
